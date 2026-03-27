097fa1a14e7601945e79e98ff185bfceea463e73
Ferlaty <davidmato330@gmail.com>
2026-03-27 17:25:00 +0100

    Initial commit from to git

---
+++ js/footer.js
@@ -0,0 +1,63 @@
+export const Footer_ = `
+<footer class="footer-classic mt-auto" role="contentinfo">
+    <div class="footer-classic__container">
+      <div class="footer-classic__main">
+
+        <!-- Column 1: Brand Information -->
+        <div class="footer-classic__brand">
+          <a href="index.html#" class="logo" aria-label="Ferlaty's official website">
+            <img src="assets/ico.png" alt="Ferlaty logo">
+          </a>
+          <p>
+            Content creation.
+          </p>
+        </div>
+
+        <!-- Column 2: Quick Links -->
+        <div class="footer-classic__links">
+          <h3 class="footer-classic__heading">Company</h3>
+          <ul>
+            <li><a href="index.html#">Home</a></li>
+            <li><a href="about_us.html#">About Us</a></li>
+            <li><a href="news.html#">News</a></li>
+            <li><a href="contact_us.html#">Contact Us</a></li>
+            <li><a href="content.html#">Content</a></li>
+            <li><a href="social_media.html#">Social Media</a></li>
+          </ul>
+        </div>
+
+        <!-- Column 3: Resources -->
+        <div class="footer-classic__links">
+          <h3 class="footer-classic__heading">Resources</h3>
+          <ul>
+            <li><a href="multiple_app_launcher.html#">Multiple App Launcher</a></li>
+            <li><a href="it_automation_toolkit.html#">IT Automation Toolkit</a></li>
+            <li><a href="https://www.youtube.com/@Ferlaty">Videos</a></li>
+            <li><a href="https://www.overtake.gg/members/italy-studio.3541341/">Assetto Corsa mods</a></li>
+          </ul>
+        </div>
+
+      </div>
+      <div class="footer-classic__bottom">
+
+        <p class="footer-classic__copyright">
+          © 2026 Ferlaty. All rights reserved. | <a href="legal.html">Legal</a>
+        </p>
+
+        <nav class="footer-classic__social" aria-label="Social Media">
+          <ul>
+            <li><a href="https://www.youtube.com/@Ferlaty" aria-label="Our YouTube account">
+                <img src="assets/yt_icon_red_digital.png" alt="Ferlaty YouTube" style="height: 40px;">
+              </a></li>
+            <li><a href="https://www.github.com/Ferlaty" aria-label="Our GitHub account">
+                <img src="assets/github-mark.png" alt="Ferlaty GitHub" style="height: 30px;">
+              </a></li>
+            <li><a href="https://www.tiktok.com/@ferlaty" aria-label="Our TikTok account">
+                <img src="assets/TikTok_Icon_Black_Circle.png" alt="Ferlaty TikTok" style="height: 35px;">
+              </a></li>
+          </ul>
+        </nav>
+
+      </div>
+    </div>
+  </footer>`;
